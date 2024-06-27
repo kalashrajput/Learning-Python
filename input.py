041cc1c7fc798042344120c76a398bfafd480b8c
Kalash Rajput <157613149+kalashrajput@users.noreply.github.com>
2024-06-28 00:08:23 +0530

    Add files via upload

---
+++ input.py
@@ -0,0 +1,12 @@
+#Using Input Function :
+#First code
+name=input("Enter you name : ");
+print(name);
+#Second code
+name=input("Enter your name : ");
+print("Welcome! be a better coder mr/ms.",name);
+#Third code
+a=int(input("Enter first number : "));
+b=int(input("Enter second number : "));
+c=a*b;
+print("Multiplication of",a,"and",b,"is",c);
