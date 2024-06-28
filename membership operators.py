c4bc24b37c4d3ba74a2337c0020410c9684f6296
Kalash Rajput <157613149+kalashrajput@users.noreply.github.com>
2024-06-29 00:12:34 +0530

    Add files via upload

---
+++ membership operators.py
@@ -0,0 +1,15 @@
+#Using Membership Operators :
+#in
+a=1;
+list=[1,2,3,4,5];
+if(a in list):
+    print("Yes, a is present");
+else:
+    print("No, a is not present");
+#not in
+a=10;
+list=[1,2,3,4,5,6];
+if(a not in list):
+    print("a is not present");
+else:
+    print("a is present");
