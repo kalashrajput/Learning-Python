fd2ea55306452e65458e1f5080fed67e3df91dcc
Kalash Rajput <157613149+kalashrajput@users.noreply.github.com>
2024-06-29 00:12:19 +0530

    Add files via upload

---
+++ identity operators.py
@@ -0,0 +1,13 @@
+#Using Identity Operators :
+#in
+a=5;
+if(type(a) is int):
+    print("Correct");
+else :
+    print("Incorrect");
+#not in
+a=5.5;
+if(type(a) is not float):
+    print("Correct");
+else:
+    print("Incorrect");
