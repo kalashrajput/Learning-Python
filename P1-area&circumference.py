3fa540b25dc5f966bb3103dfd09c68713043445b
Kalash Rajput <157613149+kalashrajput@users.noreply.github.com>
2024-06-29 02:12:12 +0530

    Add files via upload

---
+++ P1-area&circumference.py
@@ -0,0 +1,26 @@
+#Area of a rectangle :
+l=int(input("Enter length of rectangle : "));
+b=int(input("Enter breadth of rectangle :"));
+aor=l*b;
+print("Area of rectangle - ",aor);
+#Perimeter of rectangle :
+l=int(input("Enter length of rectangle : "));
+b=int(input("Enter breadth of rectangle :"));
+por=2*(l+b);
+print("Perimeter of rectangle - ",por);
+#Area of square :
+side=int(input("Enter side of square : "));
+aos=side*side;
+print("Area of square - ",aos);
+#Periemter of square :
+side=int(input("Enter side of square : "));
+pos=4*side;
+print("Perimeter of square - ",pos);
+#Area of circle :
+r=int(input("Enter radius of circle : "));
+aoc=3.14*r*r;
+print("Area of circle - ",aoc);
+#Circumference of circle :
+r=int(input("Enter radius of circle : "));
+coc=2*3.14*r;
+print("Circumference of circle - ",coc);
