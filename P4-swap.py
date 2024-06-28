e743c6c514474e4ef9dfb6083787126a94549227
Kalash Rajput <157613149+kalashrajput@users.noreply.github.com>
2024-06-29 02:12:55 +0530

    Add files via upload

---
+++ P4-swap.py
@@ -0,0 +1,10 @@
+#WAP to swap values of two numbers.
+a=int(input("Enter value of a : "));
+b=int(input("Enter value of b : "));
+c=a;
+a=b;
+b=c;
+print("------ SWAPPING OF TWO NUMBERS ------");
+print("Value of a after swap - ",a);
+print("Value of b after swap - ",b);
+
