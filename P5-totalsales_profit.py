487ea20c0f9d8ac632f3c7e5fd5ee7ce6b505122
Kalash Rajput <157613149+kalashrajput@users.noreply.github.com>
2024-06-29 02:13:06 +0530

    Add files via upload

---
+++ P5-totalsales_profit.py
@@ -0,0 +1,6 @@
+#WAP to accept total sales amount and find the profit amount @5%.
+totalsales=int(input("Enter total sales : "));
+profit=totalsales*(5/100);
+print("------ PROFIT EARNED ------");
+print("Total sales of the month - ",totalsales);
+print("Profit earned - ",profit);
