38e61600d3029e07b2b1f466731659cc324cfe7d
Kalash Rajput <157613149+kalashrajput@users.noreply.github.com>
2024-07-02 22:46:47 +0530

    Add files via upload

---
+++ if_max_2.py
@@ -0,0 +1,7 @@
+#WAP to find max between two numbers.
+a=int(input("Enter first number : "));
+b=int(input("Enter second number : "));
+if(a>b):
+    print("a is bigger");
+else:
+    print("b is bigger");
