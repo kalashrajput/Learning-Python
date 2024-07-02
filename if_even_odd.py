76efd8af817cc9e2686b502b907cf4ef662eef5a
Kalash Rajput <157613149+kalashrajput@users.noreply.github.com>
2024-07-02 22:45:51 +0530

    Add files via upload

---
+++ if_even_odd.py
@@ -0,0 +1,6 @@
+#WAP to check whether a number is odd/even.
+a=int(input("Enter Number to check if its even or odd : "));
+if(a%2==0):
+    print("The number is even");
+else:
+    print("The number is odd");
