7f65891c152cea47f2f37b06adc54c7a1cb5d3a0
Kalash Rajput <kalashrajput5725@gmail.com>
2024-07-10 00:17:50 +0530

    Added new programs

---
+++ for.py
@@ -0,0 +1,13 @@
+#WAP to print numbers from 1 to 10, 10 to 1 and 2 to 20.
+#1 to 10 -
+print("-- 1 to 10 --");
+for i in range(1,11):
+    print(i)
+#10 to 1 -
+print("-- 10 to 1 --");
+for i in range(10,0,-1):
+    print(i)
+#2 to 20 -
+print("-- 2 to 20 --");
+for i in range(2,21,2):
+    print(i)
---
+++ for_table.py
@@ -0,0 +1,4 @@
+#WAP to print table of given number.
+n=int(input("Enter a number : "));
+for i in range(1,11):
+    print(n,"*",i,"=",n*i)
